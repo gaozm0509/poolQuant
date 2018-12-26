450df7e33704aa9a49f38933aa0d5c429569163f
高泽民 <gaozm@boqii.com>
2018-12-26 16:52:03 +0800

    测试ccxt002

--- exchang_datas.py
+++ exchang_datas.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 # 获取交易所数据
 
-import ccxt
-okcoin = ccxt.okcoinusd ()
+import ccxt.async as ccx
+okcoin = ccxt.async.okcoin ()
 markets = okcoin.load_markets ()
-print (okcoin.id, markets)
+print (markets)
