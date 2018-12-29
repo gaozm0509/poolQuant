83cc6c3ca5a4564999e6b9ca9ffafd0ee3d12015
高泽民 <gaozm@boqii.com>
2018-12-29 14:47:41 +0800

    bug测试

--- exchang_datas.py
+++ exchang_datas.py
@@ -56,6 +56,7 @@ def get_price_from_key(key, market=OKEX):
     exchange = get_market(market)
     # 获取ticker信息
     ticker = exchange.fetch_ticker(key)
+    print(ticker)
     return float(ticker['info']['AskPrice'])
 
 
